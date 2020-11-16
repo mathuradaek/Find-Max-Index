535391e64929830f0a8369e60c342efb0f93a4a5
mathue <aor@outlook.com>
2020-11-16 17:59:06 +0700

    upd

--- MaxIndex.py
+++ MaxIndex.py
@@ -1,3 +1,10 @@
 
-array=[1,2,1,3,5,6,4] 
-print('Position of the maximum value is ', array.index(max(array))) 
+arr = [1,2,1,3,5,6,4]
+max_ele = arr[0]
+count, index = 0, 0
+for i in range(1, len(arr)):
+    count += 1
+    if arr[i] > max_ele:
+        max_ele = arr[i]
+        index = count
+print('Position of Maximum Value in the list', arr, 'is', index)
